ea039490696c1af76eaecdd5869700e0374ebc59
Developer SRG <shivamgarade05@gmail.com>
2026-01-07 08:36:17 +0000

    new keyword , jvm, jre , jdk

---
+++ javaIntroduction/Topics/new keyword/New.java
@@ -0,0 +1,11 @@
+public class New {
+    int x;
+        void display(){
+            x = 10;
+            System.out.println(x);
+        }
+    public static void main(String[] args) {
+        demo d = new New();
+        d.display();
+    }
+}
